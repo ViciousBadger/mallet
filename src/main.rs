6cb36c3ee71dd70dfc2b79930a9887adf55104f2
badgerson <mail@badgerson.com>
2024-07-11 13:21:57 +0200

    basic camera rotation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use bevy::prelude::*;
+use bevy::{
+    input::{keyboard::KeyboardInput, mouse::MouseMotion, ButtonState},
+    prelude::*,
+    window::{CursorGrabMode, PrimaryWindow},
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(PreUpdate, cursor_grab)
+        .add_systems(Update, camera_rotation)
         .run();
 }
 
@@ -12,14 +18,17 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-        projection: Projection::Perspective(PerspectiveProjection {
-            fov: 72.0_f32.to_radians(),
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+            projection: Projection::Perspective(PerspectiveProjection {
+                fov: 72.0_f32.to_radians(),
+                ..default()
+            }),
             ..default()
-        }),
-        ..default()
-    });
+        },
+        FirstPersonCameraController::default(),
+    ));
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(Circle::new(4.0)),
@@ -43,4 +52,50 @@ fn setup(
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
+}
+
+#[derive(Component, Default)]
+struct FirstPersonCameraController {
+    yaw_pitch: Vec2,
+}
+
+fn cursor_grab(
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+    mut input: EventReader<KeyboardInput>,
+) {
+    for event in input.read() {
+        if event.key_code == KeyCode::Tab && event.state.is_pressed() {
+            let mut window = windows.single_mut();
+
+            if window.cursor.grab_mode == CursorGrabMode::None {
+                window.cursor.grab_mode = CursorGrabMode::Locked;
+                window.cursor.visible = false;
+            } else {
+                window.cursor.grab_mode = CursorGrabMode::None;
+                window.cursor.visible = false;
+                window.cursor.visible = true;
+            }
+        }
+    }
+}
+
+fn camera_rotation(
+    mut cameras: Query<(&mut FirstPersonCameraController, &mut Transform)>,
+    mut mouse_motion: EventReader<MouseMotion>,
+) {
+    for motion in mouse_motion.read() {
+        if let Ok((mut controller, mut transform)) = cameras.get_single_mut() {
+            controller.yaw_pitch += motion.delta * 0.0022;
+
+            transform.rotation = Quat::from_euler(
+                EulerRot::YXZ,
+                -controller.yaw_pitch.x,
+                -controller.yaw_pitch.y,
+                0.0,
+            )
+        } else {
+            // TODO: Handle no controllers / too many controllers gracefully..
+            panic!("Exactly one FPCC is needed!");
+        }
+    }
 }
