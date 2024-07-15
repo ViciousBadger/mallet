8406c01c407e9817bb4430e18f037a3baa239a2b
badgerson <mail@badgerson.com>
2024-07-15 12:39:21 +0200

    refactor n stuff

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+mod camera;
+mod util;
+
 use bevy::{
     app::AppExit,
-    core::Zeroable,
-    input::{
-        keyboard::KeyboardInput,
-        mouse::{MouseButtonInput, MouseMotion, MouseWheel},
-    },
+    input::keyboard::KeyboardInput,
     prelude::*,
     window::{CursorGrabMode, PrimaryWindow},
 };
-use util::move_toward_3d;
-
-mod util;
+use camera::{
+    camera_rotation, freelook_input, freelook_input_reset, freelook_movement, FreelookCameraBundle,
+};
+use color_eyre::eyre::Result;
 
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 enum EditorState {
@@ -19,7 +19,9 @@ enum EditorState {
     Fly,
 }
 
-fn main() {
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
     App::new()
         .add_plugins(DefaultPlugins)
         .init_state::<EditorState>()
@@ -29,7 +31,8 @@ fn main() {
             (
                 editor_state_change,
                 exit_listener,
-                (camera_rotation, freelook_input).run_if(in_state(EditorState::Fly)),
+                freelook_input,
+                camera_rotation.run_if(in_state(EditorState::Fly)),
             ),
         )
         .add_systems(Update, freelook_movement)
@@ -39,30 +42,20 @@ fn main() {
             (release_mouse, freelook_input_reset),
         )
         .run();
+
+    Ok(())
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut ambient_light: ResMut<AmbientLight>,
+    // mut ambient_light: ResMut<AmbientLight>,
 ) {
     // ambient_light.color = Color::WHITE;
     // ambient_light.brightness = 1.0;
 
-    commands.spawn((
-        Camera3dBundle {
-            transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-            projection: Projection::Perspective(PerspectiveProjection {
-                fov: 72.0_f32.to_radians(),
-                ..default()
-            }),
-            ..default()
-        },
-        // TODO: Bundle these
-        Freelook::default(),
-        CameraRotator::default(),
-    ));
+    commands.spawn(FreelookCameraBundle::default());
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(Circle::new(4.0)),
@@ -112,116 +105,6 @@ fn editor_state_change(
                 EditorState::Select => EditorState::Fly,
                 EditorState::Fly => EditorState::Select,
             });
-        }
-    }
-}
-
-#[derive(Component)]
-struct Freelook {
-    target_move: Vec3,
-    velocity: Vec3,
-    speed: i32,
-}
-
-impl Default for Freelook {
-    fn default() -> Self {
-        Freelook {
-            target_move: Vec3::ZERO,
-            velocity: Vec3::ZERO,
-            speed: 5,
-        }
-    }
-}
-
-#[derive(Component, Default)]
-struct CameraRotator {
-    yaw_pitch: Vec2,
-}
-
-fn freelook_input(
-    mut q_freelook: Query<&mut Freelook>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut mouse_wheel: EventReader<MouseWheel>,
-) {
-    if let Ok(mut freelook) = q_freelook.get_single_mut() {
-        let mut raw_move = Vec3::ZERO;
-
-        if keyboard.pressed(KeyCode::KeyW) {
-            raw_move -= Vec3::Z
-        }
-        if keyboard.pressed(KeyCode::KeyA) {
-            raw_move -= Vec3::X
-        }
-        if keyboard.pressed(KeyCode::KeyS) {
-            raw_move += Vec3::Z
-        }
-        if keyboard.pressed(KeyCode::KeyD) {
-            raw_move += Vec3::X
-        }
-        if keyboard.pressed(KeyCode::KeyF) {
-            raw_move -= Vec3::Y
-        }
-        if keyboard.pressed(KeyCode::KeyR) {
-            raw_move += Vec3::Y
-        }
-
-        freelook.target_move = raw_move.normalize_or_zero();
-
-        for event in mouse_wheel.read() {
-            freelook.speed = (freelook.speed + if event.y > 0.0 { 1 } else { -1 }).clamp(1, 10);
-        }
-    }
-}
-
-fn freelook_input_reset(mut q_freelook: Query<&mut Freelook>) {
-    if let Ok(mut freelook) = q_freelook.get_single_mut() {
-        freelook.target_move = Vec3::ZERO;
-    }
-}
-
-fn freelook_movement(mut q_freelook: Query<(&mut Freelook, &mut Transform)>, time: Res<Time>) {
-    if let Ok((mut freelook, mut transform)) = q_freelook.get_single_mut() {
-        let xz_movement = freelook.target_move.xz().rotate(Vec2::from_angle(
-            -transform.rotation.to_euler(EulerRot::YXZ).0,
-        ));
-
-        let max_speed = (freelook.speed as f32).powf(1.5);
-        let accel = max_speed * 3.0;
-
-        let adjusted_move =
-            Vec3::new(xz_movement.x, freelook.target_move.y, xz_movement.y) * max_speed;
-
-        freelook.velocity = move_toward_3d(
-            freelook.velocity,
-            adjusted_move,
-            time.delta_seconds() * accel,
-        );
-
-        if freelook.velocity.length() > max_speed {
-            freelook.velocity = freelook.velocity.normalize_or_zero() * max_speed;
-        }
-
-        transform.translation += freelook.velocity * time.delta_seconds();
-    }
-}
-
-fn camera_rotation(
-    mut q_camera_rotator: Query<(&mut CameraRotator, &mut Transform)>,
-    mut mouse_motion: EventReader<MouseMotion>,
-) {
-    for motion in mouse_motion.read() {
-        if let Ok((mut rotator, mut transform)) = q_camera_rotator.get_single_mut() {
-            rotator.yaw_pitch += motion.delta * 0.0022;
-
-            transform.rotation = Quat::from_euler(
-                EulerRot::YXZ,
-                -rotator.yaw_pitch.x,
-                -rotator.yaw_pitch.y,
-                0.0,
-            )
-        } else {
-            // TODO: Handle no controllers / too many controllers gracefully..
-            panic!("Exactly one FPCC is needed!");
         }
     }
 }
