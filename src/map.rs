c87b534f88b616e5f75140e097b653d4e79bf1a3
badgerson <mail@badgerson.com>
2025-04-19 12:58:26 +0200

    map

--- src/map.rs
+++ src/map.rs
@@ -1,1 +1,49 @@
-use bevy::prelude::*;
+use bevy::math::{Mat4, Vec3};
+use serde::{Deserialize, Serialize};
+use ulid::Ulid;
+
+#[derive(Serialize, Deserialize)]
+pub struct Map {
+    pub nodes: Vec<MapNode>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MapNode {
+    pub id: Ulid,
+    pub name: String,
+    pub transform: Mat4,
+    pub kind: MapNodeKind,
+}
+
+impl MapNode {
+    pub fn new(kind: MapNodeKind) -> Self {
+        let id = Ulid::new();
+        let name = format!("{}-{}", id, kind.name());
+        let transform = Mat4::IDENTITY;
+        Self {
+            id,
+            name,
+            transform,
+            kind,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MapNodeKind {
+    Brush(Brush),
+}
+
+impl MapNodeKind {
+    pub fn name(&self) -> &'static str {
+        match self {
+            MapNodeKind::Brush(_) => "Brush",
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Brush {
+    pub start: Vec3,
+    pub end: Vec3,
+}
