d0e42ca2919e7935e00479fd1756dd77bb2a0a8e
badgerson <mail@badgerson.com>
2025-05-06 16:50:26 +0200

    more db

--- src/experimental/sled_backed_map.rs
+++ src/experimental/sled_backed_map.rs
@@ -8,48 +8,16 @@ use sled::{Config, Db, Tree};
 #[derive(Resource)]
 struct MapDb {
     db: Db,
-    meta: MapMeta,
 }
 
-const META_KEY: &[u8] = "meta".as_bytes();
 const HIST_KEY: &[u8] = "history".as_bytes();
 
-struct MapMeta(Tree);
-
-pub fn try_block<T>(closure: impl FnOnce() -> eyre::Result<T>) -> Option<T> {
-    match closure() {
-        Ok(result) => Some(result),
-        Err(err) => {
-            error!("try block failed with error\n{}", err);
-            None
-        }
-    }
-}
-
-impl MapMeta {
-    pub fn set_name(&self, name: &str) {
-        try_block(|| {
-            self.0.insert("name", name)?;
-            Ok(())
-        });
-    }
-
-    pub fn get_name(&self) -> String {
-        try_block(|| {
-            Ok(String::from_utf8(
-                self.0.get("name")?.unwrap_or_default().to_vec(),
-            )?)
-        })
-        .unwrap_or_default()
-    }
-}
+impl MapDb {}
 
 impl MapDb {
-    pub fn new_temp() -> sled::Result<Self> {
+    pub fn new_temp() -> sled::Result<MapDb> {
         let db = Config::new().temporary(true).open()?;
-        let meta = MapMeta(db.open_tree(META_KEY)?);
-        meta.set_name("hello world");
-        Ok(Self { db, meta })
+        Ok(MapDb { db })
     }
 }
 
@@ -57,31 +25,22 @@ impl MapDb {
 struct MapHistEntr {
     pub parent: u64,
     pub timestamp: i64,
-    pub delta: MapDelta,
+    pub kind: MapHistEntrKind,
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum MapDelta {
+pub enum MapHistEntrKind {
     Init,
-    AddNode {
-        node_id: u64,
-        content_id: u64,
-    },
-    ModifyNode {
-        node_id: u64,
-        content_id_before: u64,
-        content_id_after: u64,
-    },
-    RemoveNode {
-        node_id: u64,
-        content_id_before: u64,
+    NodeModify {
+        id: u64,
+        type_id: u64,
+        state_before: Option<u64>,
+        state_after: Option<u64>,
     },
 }
 
 fn test(mut commands: Commands) {
     let map = MapDb::new_temp().unwrap();
-    let name = map.meta.get_name();
-    info!("name is {}", name);
     commands.insert_resource(map);
 }
 
