0cbacd68358a46d04045dde901b42fac4c90f617
badgerson <mail@badgerson.com>
2025-05-06 16:22:41 +0200

    db insantiy

--- src/experimental/sled_backed_map.rs
+++ src/experimental/sled_backed_map.rs
@@ -1,6 +1,8 @@
 use std::str::from_utf8;
 
 use bevy::prelude::*;
+use color_eyre::eyre;
+use serde::{Deserialize, Serialize};
 use sled::{Config, Db, Tree};
 
 #[derive(Resource)]
@@ -10,16 +12,35 @@ struct MapDb {
 }
 
 const META_KEY: &[u8] = "meta".as_bytes();
+const HIST_KEY: &[u8] = "history".as_bytes();
 
 struct MapMeta(Tree);
 
+pub fn try_block<T>(closure: impl FnOnce() -> eyre::Result<T>) -> Option<T> {
+    match closure() {
+        Ok(result) => Some(result),
+        Err(err) => {
+            error!("try block failed with error\n{}", err);
+            None
+        }
+    }
+}
+
 impl MapMeta {
     pub fn set_name(&self, name: &str) {
-        self.0.insert("name", name).unwrap();
+        try_block(|| {
+            self.0.insert("name", name)?;
+            Ok(())
+        });
     }
 
     pub fn get_name(&self) -> String {
-        String::from_utf8(self.0.get("name").unwrap().unwrap().to_vec()).unwrap()
+        try_block(|| {
+            Ok(String::from_utf8(
+                self.0.get("name")?.unwrap_or_default().to_vec(),
+            )?)
+        })
+        .unwrap_or_default()
     }
 }
 
@@ -29,14 +50,32 @@ impl MapDb {
         let meta = MapMeta(db.open_tree(META_KEY)?);
         meta.set_name("hello world");
         Ok(Self { db, meta })
-        //Self(sled::confi)
     }
+}
 
-    // pub fn new(map_file: Path) -> Self {
-    //     Self(
-    //     )
-    //
-    // }
+#[derive(Serialize, Deserialize)]
+struct MapHistEntr {
+    pub parent: u64,
+    pub timestamp: i64,
+    pub delta: MapDelta,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MapDelta {
+    Init,
+    AddNode {
+        node_id: u64,
+        content_id: u64,
+    },
+    ModifyNode {
+        node_id: u64,
+        content_id_before: u64,
+        content_id_after: u64,
+    },
+    RemoveNode {
+        node_id: u64,
+        content_id_before: u64,
+    },
 }
 
 fn test(mut commands: Commands) {
